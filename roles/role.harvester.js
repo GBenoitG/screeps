50af750b4662048806dc0dc71cf9c6846b42009d
Benoit <contact@benoitguillot.fr>
2018-04-12 02:03:23 +0200

    :construction: | work on functions of roles

--- roles/role.harvester.js
+++ roles/role.harvester.js
@@ -14,6 +14,25 @@ var roleHarvester = {
             }
         }
 	}
+
+	createNewOne: function(spawnStr) {
+	    Game.spawns[spawnStr].createCreep([WORK, CARRY, MOVE], "harvester" + (getLast() + 1)) );
+	}
+
+	getHarvesters: function() {
+	    let harvesters = _.filter(Game.creeps, function(o) { return o.name.includes("harvester" });
+	    console.log("nb harvester : " + harvesters);
+	    return harvesters;
+	}
+
+	getLast: function() {
+	    return _.max(getHarvesters, function(o) { return o.name.match(/\d+/)[0] } ).name.match(/\d+/)[0];
+	}
+
+	getNb: function() {
+	    return getHarvesters.length
+	}
+
 };
 
 module.exports = roleHarvester;
--- roles/role.upgrader.js
+++ roles/role.upgrader.js
@@ -2,12 +2,34 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    /*if(Game.spawns['Spawn1'].energy == Game.spawns['Spawn1'].energyCapacity) {
-                if(creep.transfer(creep.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
-                }
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
             }
-            else */
+        }
+        else {
+            if(creep.transfer(Game.room.controller, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.room.controller);
+            }
+        }
+	}
+
+	createNewOne: function(spawnStr) {
+	    Game.spawns[spawnStr].createCreep([WORK, CARRY, MOVE], "upgrader" + (getLast() + 1)) );
+	}
+
+	getUpgraders: function() {
+	    let upgraders = _.filter(Game.creeps, function(o) { return o.name.includes("upgrader") });
+	    console.log("nb upgrader : " + upgraders);
+	    return upgraders;
+	}
+	getLast: function() {
+	    return _.max(getUpgraders(), function(o) { return o.name.match(/\d+/)[0] } ).name.match(/\d+/)[0];
+	}
+
+	getNb: function() {
+	    return getUpgraders().length
 	}
 };
 
